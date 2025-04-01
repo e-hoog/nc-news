a3b7d19e2bbec2498e26b8e5667db05823f70a6b
e-hoog <eddiehoog@hotmail.com>
2025-04-01 16:33:20 +0100

    add pagination cmponent to all articles page

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,10 +1,12 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import ArticleCard from "./ArticleCard";
 import useApiRequest from "./useApiRequest";
 import { getAllArticles } from "./utils/api";
 import { CircularProgress } from "@mui/material";
+import Pagination from "./Pagination";
 
 function ArticleList({ sort_by, perPage }) {
+  const [page, setPage] = useState(1);
   const apiParams = useMemo(() => {
     return { sort_by };
   }, []);
@@ -19,11 +21,22 @@ function ArticleList({ sort_by, perPage }) {
         <CircularProgress />
       ) : (
         articles.map((article, index) => {
-          if (index < perPage) {
+          if (
+            index + 1 <= perPage * page &&
+            index + 1 >= perPage * page - (perPage - 1)
+          ) {
             return <ArticleCard key={article.article_id} article={article} />;
           }
         })
       )}
+      {perPage > 3 && !isLoading ? (
+        <Pagination
+          items={articles.length}
+          perPage={perPage}
+          page={page}
+          setPage={setPage}
+        ></Pagination>
+      ) : null}
     </section>
   );
 }
