3a3de0a74f6a31c928ff22f9feecad90f4bb23ca
e-hoog <eddiehoog@hotmail.com>
2025-04-02 09:43:07 +0100

    use searchParams to grab article id from url

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,22 @@
+import { useParams, useSearchParams } from "react-router-dom";
+
 function Article() {
-  return <h2>Article Page</h2>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { article_id } = useParams();
+  return (
+    <section>
+      <h1>Article Title</h1>
+      <p>Date Posted</p>
+      <p>Author</p>
+      <p>Topic</p>
+      <p>
+        Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+        distinctio quam culpa optio autem. Qui sint saepe quasi error,
+        exercitationem corporis sapiente repellendus voluptate, recusandae nihil
+        quidem pariatur ab quaerat.
+      </p>
+    </section>
+  );
 }
 
 export default Article;
