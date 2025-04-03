138c784e9a4a3e29f52dae0dba84affdac1d842b
e-hoog <eddiehoog@hotmail.com>
2025-04-03 11:12:16 +0100

    add Votes component with optimistic rendering of article votes

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,61 +3,76 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
-import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Votes from "./Votes";
+import { Alert } from "@mui/material";
 
 function ArticleCard({ article }) {
-  const buttons = [
-    <Button key="vote-up">⬆</Button>,
-    <Button key="vote-down">⬇</Button>,
-  ];
   const navigate = useNavigate();
   function handleCardClick() {
     navigate(`/articles/${article.article_id}`);
   }
+  const [voteError, setVoteError] = useState(null);
   return (
-    <div className="article-card-container">
-      <Box
-        sx={{
-          display: "block",
-          "& > *": {
-            m: 1,
-          },
-        }}
-      >
-        <ButtonGroup orientation="vertical" aria-label="Vertical button group">
-          {buttons}
-        </ButtonGroup>
-      </Box>
-      <Card sx={{ width: 345 }}>
-        <CardActionArea onClick={handleCardClick}>
-          <CardMedia
-            sx={{ height: 140 }}
-            image={article.article_img_url}
-            title={article.title}
-          />
-          <CardContent>
-            <Typography
-              gutterBottom
-              sx={{ color: "text.secondary", fontSize: 14 }}
-            >
-              {article.topic.charAt(0).toUpperCase() + article.topic.slice(1)}
-            </Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              {article.title}
-            </Typography>
-            <Typography sx={{ color: "text.secondary" }}>
-              Author: {article.author}
-            </Typography>
-            <Typography variant="body3" sx={{ color: "text.secondary" }}>
-              {article.created_at.slice(0, 10)}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </div>
+    <>
+      {" "}
+      {voteError ? (
+        <div>
+          <Alert severity="error">{voteError}</Alert>
+        </div>
+      ) : null}
+      <div className="article-card-container">
+        <Box
+          sx={{
+            display: "block",
+            "& > *": {
+              m: 1,
+            },
+          }}
+        >
+          <ButtonGroup
+            className="votes-vertical"
+            orientation="vertical"
+            aria-label="Vertical button group"
+          >
+            <Votes
+              article_id={article.article_id}
+              votes={article.votes}
+              setVoteError={setVoteError}
+            />
+          </ButtonGroup>
+        </Box>
+        <Card sx={{ width: 345 }}>
+          <CardActionArea onClick={handleCardClick}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image={article.article_img_url}
+              title={article.title}
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                sx={{ color: "text.secondary", fontSize: 14 }}
+              >
+                {article.topic.charAt(0).toUpperCase() + article.topic.slice(1)}
+              </Typography>
+              <Typography gutterBottom variant="h5" component="div">
+                {article.title}
+              </Typography>
+              <Typography sx={{ color: "text.secondary" }}>
+                Author: {article.author}
+              </Typography>
+              <Typography variant="body3" sx={{ color: "text.secondary" }}>
+                {article.created_at.slice(0, 10)}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Votes.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { patchArticleVotesById } from "./utils/api";
+import { Button } from "@mui/material";
+
+function Votes({ votes, article_id, setVoteError }) {
+  const [votesCount, setVotesCount] = useState(votes);
+  function handleUpvote() {
+    console.log("+1");
+    setVotesCount((currentVotesCount) => currentVotesCount + 1);
+    setVoteError(null);
+    patchArticleVotesById(article_id, 1).catch((err) => {
+      setVotesCount((currentVotesCount) => currentVotesCount - 1);
+      setVoteError("Your vote was not successful. Please try again!");
+    });
+  }
+  function handleDownvote() {
+    console.log("-1");
+    setVotesCount((currentVotesCount) => currentVotesCount - 1);
+    setVoteError(null);
+    patchArticleVotesById(article_id, -1).catch((err) => {
+      setVotesCount((currentVotesCount) => currentVotesCount + 1);
+      setVoteError("Your vote was not successful. Please try again!");
+    });
+  }
+
+  return (
+    <div>
+      <Button onClick={handleUpvote} key={article_id + "vote-up"}>
+        ⬆
+      </Button>
+      <p className="votes">{votesCount}</p>
+      <Button onClick={handleDownvote} key={article_id + "vote-down"}>
+        ⬇
+      </Button>
+    </div>
+  );
+}
+
+export default Votes;
